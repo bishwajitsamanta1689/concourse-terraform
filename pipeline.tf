c05652d7eba5d793fcc02d561ae6369bfe6c0b9b
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-28 22:04:02 +0530

    Added Path

--- pipeline.tf
+++ pipeline.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("ci-cd-pipeline-269615-4f66a8568f70.json")}"
+  credentials = "${file("../ci-cd-pipeline-269615-4f66a8568f70.json")}"
   project = "ci-cd pipeline"
   region = "us-central1"
 }
