3032b2d2ee1b28780259663a2adcce3008b5f282
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-28 22:08:25 +0530

    Added a Concourse Instance Configuration

--- pipeline.tf
+++ pipeline.tf
@@ -3,3 +3,15 @@ provider "google" {
   project = "ci-cd pipeline"
   region = "us-central1"
 }
+
+resource "google_compute_instance" "concourse" {
+  name = "concourse"
+  machine_type = "n1-standard-1"
+  zone = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+}
