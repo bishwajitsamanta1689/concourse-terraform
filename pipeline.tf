4054db89108786fb7547a16a14cbdd7d19086428
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-28 22:41:22 +0530

    Project Name Added

--- pipeline.tf
+++ pipeline.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = "${file("../ci-cd-pipeline-269615-4f66a8568f70.json")}"
-  project = "ci-cd pipeline"
+  project = "ci-cd-pipeline-269615"
   region = "us-central1"
 }
 
